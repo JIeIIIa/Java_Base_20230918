59ea5f1d9e96285631316344d85b5a478bed1210
Oleksii Onishchenko <stealth@bigmir.net>
2023-10-26 20:18:08 +0300

    Lecture-12 - JUnit testing: manual adding

--- src/ua/hillel/lessons/lecture12/CalculatorTest.java
+++ src/ua/hillel/lessons/lecture12/CalculatorTest.java
@@ -1,33 +1,31 @@
 package ua.hillel.lessons.lecture12;
 
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+    @Test
     public void factorialOfZeroTest() {
         Calculator calculator = new Calculator();
         System.out.println("1 == " + calculator.factorial(0));
     }
 
 
+    @Test
     public void factorialOfTwo() {
         Calculator calculator = new Calculator();
         System.out.println("2 == " + calculator.factorial(2));
     }
 
 
+    @Test
     public void factorialOfTen() {
         Calculator calculator = new Calculator();
         System.out.println("3628800 == " + calculator.factorial(10));
     }
 
+    @Test
     public void factorialOfOne() {
         Calculator calculator = new Calculator();
         System.out.println("1 == " + calculator.factorial(1));
-    }
-
-    public static void main(String[] args) {
-        CalculatorTest instance = new CalculatorTest();
-        instance.factorialOfZeroTest();
-        instance.factorialOfOne();
-        instance.factorialOfTwo();
-        instance.factorialOfTen();
     }
 }
