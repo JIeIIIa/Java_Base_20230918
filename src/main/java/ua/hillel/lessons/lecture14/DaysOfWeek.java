3bb7e577c579855147793d4ba6f553bf9f859fb8
Oleksii Onishchenko <stealth@bigmir.net>
2023-11-02 19:42:15 +0200

    Lecture-14 - Enum (part-1)

--- src/main/java/ua/hillel/lessons/lecture14/DaysOfWeek.java
+++ src/main/java/ua/hillel/lessons/lecture14/DaysOfWeek.java
@@ -4,16 +4,15 @@ import java.util.Objects;
 
 public class DaysOfWeek {
     public static void main(String[] args) {
-        Day day = Day.MONDAY;
+        Day day = Day.WEDNESDAY;
         DaysOfWeek daysOfWeek = new DaysOfWeek();
 
         System.out.println("Recommendation for " + day + " is:");
         System.out.println(daysOfWeek.giveRecommendation(day));
-        System.out.println(daysOfWeek.giveRecommendation(Day.MONDAY));
     }
 
-    public String giveRecommendation(Day day) {
-        if (Day.MONDAY.equals(day)) {
+    String giveRecommendation(Day day) {
+        if (Day.MONDAY.equals(day) || Day.WEDNESDAY == day) {
             return "Go to work!";
         } else if (Day.SUNDAY.equals(day)) {
             return "Have a rest)";
@@ -22,49 +21,50 @@ public class DaysOfWeek {
     }
 }
 
-class Day {
-    static final Day MONDAY = new Day("MONDAY", 1);
-    static final Day SUNDAY = new Day("SUNDAY", 7);
+enum Day {
+    MONDAY,
+    WEDNESDAY,
+    SUNDAY;
 
-    private final String name;
-    private final int order;
+//    private final String name;
+//    private final int order;
 
-    private Day(String name, int order) {
-        this.name = name;
-        this.order = order;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getOrder() {
-        return order;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Day day = (Day) o;
-        return order == day.order &&
-                ((name == day.name) || (name != null && name.equalsIgnoreCase(day.name)));
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, order);
-    }
-
-    @Override
-    public String toString() {
-        return "Day{" +
-                "name='" + name + '\'' +
-                ", order=" + order +
-                '}';
-    }
+//    private Day(String name, int order) {
+//        this.name = name;
+//        this.order = order;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getOrder() {
+//        return order;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) {
+//            return true;
+//        }
+//        if (o == null || getClass() != o.getClass()) {
+//            return false;
+//        }
+//        Day day = (Day) o;
+//        return order == day.order &&
+//                ((name == day.name) || (name != null && name.equalsIgnoreCase(day.name)));
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, order);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Day{" +
+//                "name='" + name + '\'' +
+//                ", order=" + order +
+//                '}';
+//    }
 }
