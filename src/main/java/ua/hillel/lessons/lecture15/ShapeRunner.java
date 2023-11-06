b08c91efdd71c6630058246c7b7950d916558ee8
Oleksii Onishchenko <stealth@bigmir.net>
2023-11-06 19:10:40 +0200

    Lecture-15 - Local inner classes

--- src/main/java/ua/hillel/lessons/lecture15/ShapeRunner.java
+++ src/main/java/ua/hillel/lessons/lecture15/ShapeRunner.java
@@ -4,7 +4,24 @@ import ua.hillel.lessons.lecture15.Shape.Edge;
 
 public class ShapeRunner {
     public static void main(String[] args) {
-        nestedClasses();
+//        nestedClasses();
+        localInnerClass();
+    }
+
+    private static void localInnerClass() {
+        class BoldEdge extends Edge {
+            public BoldEdge(int length, String color) {
+                super(length, color);
+            }
+
+            @Override
+            public String toString() {
+                return "I'm BoldEdge{" + super.toString() + "}";
+            }
+        }
+
+        BoldEdge edge = new BoldEdge(42, "green");
+        System.out.println("edge = " + edge);
     }
 
     private static void nestedClasses() {
