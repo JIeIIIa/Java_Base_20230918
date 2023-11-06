4402c11484912228f8a48b66cb884a530c7d81c4
Oleksii Onishchenko <stealth@bigmir.net>
2023-11-06 20:29:55 +0200

    Lecture-15 - Try with finally

--- src/main/java/ua/hillel/lessons/lecture15/exception/ExceptionDemo.java
+++ src/main/java/ua/hillel/lessons/lecture15/exception/ExceptionDemo.java
@@ -9,18 +9,24 @@ public class ExceptionDemo {
             System.out.println("Wrong data. Please try again...");
         } catch (CalculationException | RuntimeException e) {
             System.out.println("Caught an exception: " + e.getClass() + " with message " + e.getMessage());
+        } finally {
+            System.out.println("Send message...");
         }
 
         System.out.println("Done");
     }
 
     public static int calculate(int a, int b) throws CalculationException {
-        if (a == 0) {
-            throw new ArithmeticException("a is zero");
-        } else if (b == 0) {
-            throw new CalculationException("You are trying to divide by zero");
-        } else if (b < 0) {
-            throw new WrongDataException("Wrong input data: b == " + b);
+        try {
+            if (a == 0) {
+                throw new ArithmeticException("a is zero");
+            } else if (b == 0) {
+                throw new CalculationException("You are trying to divide by zero");
+            } else if (b < 0) {
+                throw new WrongDataException("Wrong input data: b == " + b);
+            }
+        } finally {
+            System.out.println("Log actions");
         }
         System.out.println("Calculating...");
         return a / b;
